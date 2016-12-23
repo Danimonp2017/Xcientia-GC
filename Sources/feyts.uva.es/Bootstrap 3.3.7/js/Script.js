b6d707d8b7d1b81c7fd0c5c3d64614f0243396d4
Daniel Saavedra <danielsaavedram@hotmail.com>
2016-12-23 15:47:13 -0500

    Missing files and popover functionality test

--- Sources/feyts.uva.es/Bootstrap 3.3.7/js/Script.js
+++ Sources/feyts.uva.es/Bootstrap 3.3.7/js/Script.js
@@ -4,8 +4,9 @@ $(document).ready(function () {
 	updateLetter("a");
 	currentLetter="#a";
 	$("#a").addClass(" active");
-	$('[data-toggle="popover"]').popover();
 });
+
+
 $(document).on("click", ".letter", function() {
 	var name="#"+this.id+"";
 	updateLetter(this.id);
@@ -20,13 +21,14 @@ var updateLetter = function (letter) {
 	$("#content").load("/Sources/feyts.uva.es/pa	ges/"+letter+".html");
 };
 
-
-
 function copyToClipboard(element) {
-	$("button[rel=popover]").popover();
 	var $temp = $("<input>");
 	$("body").append($temp);
 	$temp.val($(element).text()).select();
 	document.execCommand("copy");
 	$temp.remove();
 }
+
+$(function () {
+	$('[data-toggle="popover"]').popover();
+})
