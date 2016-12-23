6e88b78643989571ca049f030ad440d5a0919e12
Daniel Saavedra <danielsaavedram@hotmail.com>
2016-12-23 15:12:45 -0500

    Changed Bootstrap version, added templates of a through z and included info on a through f

--- Sources/feyts.uva.es/Bootstrap 3.3.7/js/Script.js
+++ Sources/feyts.uva.es/Bootstrap 3.3.7/js/Script.js
@@ -3,6 +3,8 @@ var currentLetter = ""
 $(document).ready(function () {
 	updateLetter("a");
 	currentLetter="#a";
+	$("#a").addClass(" active");
+	$('[data-toggle="popover"]').popover();
 });
 $(document).on("click", ".letter", function() {
 	var name="#"+this.id+"";
@@ -15,5 +17,16 @@ $(document).on("click", ".letter", function() {
 var updateLetter = function (letter) {
 	$("title").html("Glosario Científico - " + letter);
 	$(".current-letter").html(letter.toUpperCase());
-	$("#content").load("/Sources/feyts.uva.es/pages/"+letter+".html");
+	$("#content").load("/Sources/feyts.uva.es/pa	ges/"+letter+".html");
 };
+
+
+
+function copyToClipboard(element) {
+	$("button[rel=popover]").popover();
+	var $temp = $("<input>");
+	$("body").append($temp);
+	$temp.val($(element).text()).select();
+	document.execCommand("copy");
+	$temp.remove();
+}
